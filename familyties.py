0e3bf4d778c4e5d6a7648f8a84aa6fdc1eb0ee9e
Jacobwj04 <32894@ma-web.nl>
2021-09-23 12:47:03 +0200

    new

---
+++ familyties.py
@@ -0,0 +1,11 @@
+line1 = "Ik ben Jacob,"
+line2 = "ik ben 16 jaar oud."
+line3 = "Ik wwon in zaandam"
+line4 = "en woon ik 12,2 km van school."
+line5 = "Ik heb een twee zussen."
+line6 = " Ik heb twee honden."
+
+print(line1 + line2 + line3 + line4)
+print(line5 + line6)
+
+
