3e2177448d5ede5ad50192bdf166f2f634a24095
Jacobwj04 <32894@ma-web.nl>
2021-09-27 12:28:02 +0200

    new

---
+++ LogicalOp.py
@@ -0,0 +1,19 @@
+name = "Jacob Wassall Jamieson"
+job = "astonaut"
+monyInAcount = 5000
+
+def BuyABrandNewRocket():
+    for Index in range(1000):
+        print(":")
+        
+if name == "Jacob Wassall Jamieson" and job != None or moneyInAcount < 10000:
+    BuyABrandNewRocket()
+
+
+
+
+
+
+
+
+
