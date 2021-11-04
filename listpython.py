f2e119aa3aeecba44d2292de1d0acea49ca4c0ac
Jacobwj04 <32894@ma-web.nl>
2021-11-04 11:47:07 +0100

    new opdracht

---
+++ listpython.py
@@ -0,0 +1,11 @@
+floatlist = [4.3, 6.7, 5.7, 8.1, 5.8, 'apple', 'aardbei', 3, 6, 8, 6, 6, 6]
+
+
+floatlist.pop(3)
+print(floatlist)
+
+x = floatlist.count(6)
+print(x)
+
+floatlist.insert(4, "5.7")
+print(floatlist)
